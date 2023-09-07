8770fe96d20dbcfa39a2004f2e4536fa7bca2670
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-09-07 22:39:22 +0300

    add delete tak feature

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,6 +1,7 @@
 import {Todo, TypedColumn} from "@/typings";
 import {DraggableProvidedDraggableProps, DraggableProvidedDragHandleProps} from "react-beautiful-dnd";
 import {XCircleIcon} from "@heroicons/react/24/solid";
+import {useBoardStore} from "@/store/BoardStore";
 
 type Props = {
     todo: Todo;
@@ -12,6 +13,8 @@ type Props = {
 }
 
 const TodoCard = ({todo, index, id, innerRef, draggableProps, dragHandleProps}: Props) => {
+    const deleteTask = useBoardStore((state) => state.deleteTodo)
+
     return (
         <div
             className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -21,7 +24,10 @@ const TodoCard = ({todo, index, id, innerRef, draggableProps, dragHandleProps}: 
         >
             <div className="flex justify-between items-center p-5">
                 <p>{todo.title}</p>
-                <button className="text-red-500 hover:text-red-600">
+                <button
+                    onClick={() => deleteTask(index, todo, id)}
+                    className="text-red-500 hover:text-red-600"
+                >
                     <XCircleIcon className="ml-5 h-8 w-8"/>
                 </button>
             </div>
