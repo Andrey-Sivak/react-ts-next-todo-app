ac3d7b821853120e41f1782569b1162e57e19c4a
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-09-18 20:27:04 +0300

    remove unnecessary object

--- typings.ts
+++ typings.ts
@@ -27,24 +27,3 @@ export interface Image {
     bucketId: string;
     fileId: string;
 }
-
-export const settings = [
-    {
-        id: ETypedColumn.Todo,
-        title: 'To Do',
-        description: 'A new task to be completed',
-        color: 'bg-red-500'
-    },
-    {
-        id: ETypedColumn.InProgress,
-        title: 'In Progress',
-        description: 'A task that is currently being worked on',
-        color: 'bg-yellow-500'
-    },
-    {
-        id: ETypedColumn.Done,
-        title: 'Done',
-        description: 'A task that has been completed',
-        color: 'bg-green-500'
-    }
-]
